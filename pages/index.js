3f26756236f10266d95a40eae0b13ca49b05be7b
mild20091931 <mild20091931@gmail.com>
2018-03-19 23:08:26 +0700

    [change] pages/index

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,20 @@ import React from 'react'
 import Custom from '../components/custom/index'
 
 export default class Index extends React.Component {
-    render() {
-        return (
-            <Custom />
-        )
-    }
+  render() {
+    return (
+      <html>
+        <head>
+          <title>Flashlight</title>
+          <script src="static/EQCSS-polyfills.min.js" />
+          <script src="static/EQCSS.min.js" />
+          <script src="static/script.js" />
+          <link rel="stylesheet" type="text/css" href="static/style.css" />
+        </head>
+        <body>
+          <Custom />
+        </body>
+      </html>
+    )
+  }
 }
