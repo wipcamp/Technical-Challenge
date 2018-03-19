5870695531b350e8f6a03c604a91430f59e1772c
DESKTOP-13DH8UD\papop <papop.ru@mail.kmutt.ac.th>
2018-03-19 19:05:37 +0700

    test2

--- components/custom/index.js
+++ components/custom/index.js
@@ -1,12 +1,51 @@
 import React from 'react'
 
-export default class Custom extends React.Component {
-    state = {  }
-    render() {
-        return (
-            <div>
-                Custom Component!
-            </div>
-        )
+export default class Animal extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        animationName: ''
+      };
     }
-}
+    
+    clickHdl() {
+      let styleSheet = document.styleSheets[0];
+   
+      let animationName = `animation${Math.round(Math.random() * 100)}`;
+      
+      let keyframes =
+      `@-webkit-keyframes ${animationName} {
+          10% {-webkit-transform:translate(${Math.random() * 300}px, ${Math.random() * 300}px)} 
+          90% {-webkit-transform:translate(${Math.random() * 300}px, ${Math.random() * 300}px)}
+          100% {-webkit-transform:translate(${Math.random() * 300}px, ${Math.random() * 300}px)}
+      }`;
+   
+      styleSheet.insertRule(keyframes, styleSheet.cssRules.length);
+      
+      this.setState({
+        animationName: animationName
+      });
+    }
+    
+    render () {
+      let style = {
+        animationName: this.state.animationName,
+        animationTimingFunction: 'ease-in-out',
+        animationDuration: '0.6s',
+        animationDelay: '0.0s',
+        animationIterationCount: 1,
+        animationDirection: 'normal',
+        animationFillMode: 'forwards'
+      };
+      
+      return (
+        <div>
+          <button type="button" onClick={this.clickHdl.bind(this)}>Animation!</button>
+          <div className="box" style={style}></div>
+        </div>
+  
+      );
+    }
+  }
+  
+  React.render(<Animal/>, document.body);
