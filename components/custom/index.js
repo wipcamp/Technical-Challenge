aa34d79e1d5f62c4b4b1042ea1b8b19d1730e83a
JewlyTwin <l2jew555@hotmail.com>
2018-03-19 17:04:56 +0700

    [ADD] Init Profile

---
+++ components/custom/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class Custom extends React.Component {
+    state = {  }
+    render() {
+        return (
+            <div>
+                Custom Component!
+            </div>
+        )
+    }
+}
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Custom from '../components/custom/index'
+
+export default class Index extends React.Component {
+    render() {
+        return (
+            <Custom />
+        )
+    }
+}
