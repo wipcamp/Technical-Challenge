1d6157da31305d8df0de31e55d18e60cf19433ad
DESKTOP-13DH8UD\papop <papop.ru@mail.kmutt.ac.th>
2018-03-22 20:21:08 +0700

    test1

--- components/custom/index.js
+++ components/custom/index.js
@@ -1,12 +1,53 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Box = styled.div`
+    border: 2px solid #000;
+
+`
 
 export default class Custom extends React.Component {
-    state = {  }
+    constructor() {
+        super();
+        this.state = {
+          fullName:""
+        }
+      }
+      handleFullNameChange = (e) => {
+        this.setState({
+          fullName: e.target.value
+        })
+      }
+      handleSubmit = (e) => {
+        e.preventDefault();
+      }
+
     render() {
         return (
             <div>
-                Custom Component!
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="fullName">Full Name</label>
+            <input
+              type="text"
+              value={this.state.fullName}
+              onChange={this.handleFullNameChange}
+              name="fullName" />
+          <input type="submit" value="Submit" />
+          
+        </form>
+        {console.log(this.state.fullName)}
+        <div>
+            {this.state.fullName}
+        </div>
+        {/* <div>{fullName.map((data,i)=>(
+            <div key={i}>
+                <h1 dangerouslySetInnerHTML={{__html: data.q}}/>
             </div>
+            
+         ))}
+        
+        </div> */}
+      </div>
         )
     }
 }
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Custom from '../components/custom/index'
 export default class Index extends React.Component {
     render() {
         return (
-            <Custom />
+            <Custom/>
         )
     }
 }
