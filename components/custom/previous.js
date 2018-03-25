9fb5ae12f977436c16c2b19bb5d178e138d8549d
gnbiiie3 <gnbiiie3@hotmail.com>
2018-03-25 09:42:10 +0700

    [create] 2 sections

---
+++ components/custom/previous.js
@@ -0,0 +1,12 @@
+import React from 'React'
+
+export default class Custom extends React.Component {
+    state = {}
+    render() {
+        return (
+            <div>
+                YOU READY?
+            </div>
+        )
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
+
 import Custom from '../components/custom/index'
+import Introduce from '../components/custom/introduce'
+import Previous from '../components/custom/previous'
+
 
 export default class Index extends React.Component {
   render() {
     return (
-          <Custom />
+      <div>
+        <Introduce/>
+        <Previous/>
+        <Custom />  
+      </div>
     )
   }
 }
