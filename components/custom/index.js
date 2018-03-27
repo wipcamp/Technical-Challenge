f15cd654f58b572844abb1ba42db088cab7ff672
gnbiiie3 <gnbiiie3@hotmail.com>
2018-03-27 17:27:08 +0700

    [change] จัดโครงเซคชั่นแรก

--- components/custom/index.js
+++ components/custom/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 import Bg from '../custom/Bg';
+import Box from '../custom/Box'
+
+const Img = styled.img`
+  width: 30%;
+  margin-top: 7%;
+  margin-right:17%;
+  position: absolute;
+  right: 0;
+`
 
 export default class Custom extends React.Component {
   state = {}
   render() {
     return (
-      <Bg className="container-fulid">
-        <div className="row">
-        <div className="col-12">
-          <img src="/static/img/hanufire1.png"  className="rounded mx-auto d-block"/>
-       </div>
-       </div>
+      <Bg className="container-fulid d-flex justify-content-center align-items-center">
+        <Box className="" text="YoooooooO!"></Box>
+        <Img src="/static/img/hanufire1.png" />
       </Bg>
     )
   }
