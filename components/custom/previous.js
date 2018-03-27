f9d799f052a95011664a0212de34ef6059ddb927
gnbiiie3 <gnbiiie3@hotmail.com>
2018-03-27 17:27:52 +0700

    [remove] object เซคชั่นที่ 3

--- components/custom/previous.js
+++ components/custom/previous.js
@@ -2,17 +2,26 @@ import React from 'react';
 import styled from 'styled-components';
 import Bg from '../custom/Bg';
 
+
+// const Img = styled.img`
+//   width: 30%;
+//   margin-top: 5%;
+//   margin-left:17%;
+//   position: absolute;
+//   left: 0;
+// `
+
 export default class Previous extends React.Component {
-  state = {}
-  render() {
-    return (
-      <Bg className="container-fulid">
-        <div className="row">
-        <div className="col-12">
-          <img src="/static/img/todfire1.png"  className="rounded mx-auto d-block"/>
-       </div>
-       </div>
-      </Bg>
-    )
-  }
+    state = {}
+    render() {
+        return (
+            <Bg className="container-fulid">
+                <div className="row">
+                    <div className="col-12">
+                        {/* <Img src="/static/img/todfire1.png" className="" /> */}
+                    </div>
+                </div>
+            </Bg>
+        )
+    }
 }
