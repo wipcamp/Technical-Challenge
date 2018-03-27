6ad78bcc9c5380d5bd0872c745cd7f57ef49af8c
gnbiiie3 <gnbiiie3@hotmail.com>
2018-03-27 17:27:30 +0700

    [change] จัดโครงเซคชั่นที่ 2

--- components/custom/introduce.js
+++ components/custom/introduce.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
 import Bg from '../custom/Bg';
+import Box from '../custom/Box'
+
+const Img = styled.img`
+  width: 30%;
+  margin-left:20%;
+  position: absolute;
+  left: 0;
+  z-index: 1;
+`
 
 export default class Introduce extends React.Component {
-  state = {}
-  render() {
-    return (
-      <Bg className="container-fulid">
-         <div className="row">
-        <div className="col-12">
-          <img src="/static/img/todfire1.png"  className="rounded mx-auto d-block"/>
-       </div>
-       </div>
-      </Bg>
-    )
-  }
+    state = {}
+    render() {
+        return (
+            <Bg className="container-fulid d-flex justify-content-center align-items-center">
+                <Box className="ml-5" text="YOOOOOOOOOOOOOOOOOOOOOOOOOOOOO!"></Box>
+                <Img src="/static/img/todfire1.png" className="" />
+            </Bg>
+        )
+    }
 }
