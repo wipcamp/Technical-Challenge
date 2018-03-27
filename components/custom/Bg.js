f8235360a2762119fe7ef1c6513530bcef8ae93a
JOKNOI <panupong.pj15@mail.kmutt.ac.th>
2018-03-28 00:42:03 +0700

    [change] แก้ div ที่ให้เป็นพื้นหลัง

--- components/custom/Bg.js
+++ components/custom/Bg.js
@@ -1,11 +1,6 @@
 import styled from 'styled-components'
 
 const Bg = styled.section`
-  background-image: url("/static/img/bg.png");
-  background-position: center center;
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  background-size: cover;
   letter-spacing: 0.05em;
   overflow-x: hidden;
   overflow-y: hidden;
